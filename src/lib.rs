eff3bfbc6be576abd4264f1e320d53323486c114
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-15 04:34:48 +1100

    Add barebones program

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 #![no_std]
 #![no_main]
 
+// import panic handler for the final binary
+use neutronkern::panic;
+
 /*
     Entry point for the Kernel
 */
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 
 
 /*
