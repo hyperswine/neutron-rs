063bdc0b1f0e6fd3898256f4e2fc5861f58fe25e
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-22 22:37:01 +1100

    Yes

--- full-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# clean up previous build
-rm -rf build && mkdir build
-
-# compile rust
-cargo build
-# link objects
-riscv64-unknown-elf-ld -T support/arch/riscv64/linker.ld -nostdlib target/riscv64gc-unknown-none-elf/debug/libneutronkern.rlib build/entry.o -o build/kernel.elf
--- src/drivers/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod video;
-pub mod keyboard;
