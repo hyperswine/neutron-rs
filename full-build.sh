ad5248b3cd39bc9206434f4aaa2e9ef5c180c342
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-22 19:43:34 +1100

    well 2

--- full-build.sh
+++ full-build.sh
@@ -8,4 +8,4 @@ cargo rustc -- --emit=obj
 # assemble assembly
 riscv64-unknown-elf-as -c support/arch/riscv64/entry.S -o build/entry.o
 # link objects
-riscv64-unknown-elf-ld -T support/arch/riscv64/linker.ld -nostdlib target/debug/deps/*.o build/entry.o -o build/kernel.elf
+riscv64-unknown-elf-ld -T support/arch/riscv64/linker.ld -nostdlib target/riscv64gc-unknown-none-elf/debug/deps/*.o build/entry.o -o build/kernel.elf
