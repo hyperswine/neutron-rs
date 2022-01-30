814a4b9396c858cca741dd512b0665f6bb29bf1a
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-30 12:40:17 +1100

    yes

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
+use arcboot::{Arch, Build}
+
 fn main() {
     // assemble
-    
+    let build = Build::new(Arch::Riscv64);
+    build.assemble("asm/riscv64/boot.S", "build/boot.o")
+        .link(&["build/boot.o", "deps/libneutronkern.a"], "link/riscv64/linker.ld", "build/kernel.elf")
+        .clean();
 }
