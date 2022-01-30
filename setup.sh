1694f4339f340cdb978752647e0fe4ff04b802b6
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-01-30 15:10:19 +1100

    Updates

--- setup.sh
+++ setup.sh
@@ -5,5 +5,23 @@ mkdir build
 cargo component add rust-src
 cargo target add riscv64gc-unknown-none-elf
 
-# build for riscv
-cargo brv | bash
+##
+# SPECTRO
+##
+
+# install extra arc tools
+cargo install arcboot
+# build for riscv64
+cargo arcboot build spectro --test 
+# run the test cfg. Note, `cargo arcboot test spectro` does the two commands together
+cargo arcboot run --test
+
+# when ready, build and run the release config
+cargo arcboot run --release
+
+##
+# PI4B
+##
+
+# build for aarch64
+cargo arcboot build aarch64
