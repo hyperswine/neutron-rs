d6feae5ca58781c731d599afc7c11bc4d6e5100f
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-08 04:42:03 +1100

    Rem redundant mem module for now

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,7 +1,10 @@
-#[cfg(target_arch = "riscv64")]
-#[path = "../kernel/arch/riscv64gc/mod.rs"]
-pub mod riscv64gc;
+// SHOULD NOT BE REQUIRED SINCE GLOBAL ALLOC HANDLER DEFINED WILL USUALLY BE FINE
+// BUT VM (virtual memory/paging) stuff may require
 
-#[cfg(target_arch = "aarch64")]
-#[path = "../kernel/arch/aarch64/mod.rs"]
-pub mod aarch64;
+// #[cfg(target_arch = "riscv64")]
+// #[path = "../kernel/arch/riscv64gc/mod.rs"]
+// pub mod riscv64gc;
+
+// #[cfg(target_arch = "aarch64")]
+// #[path = "../kernel/arch/aarch64/mod.rs"]
+// pub mod aarch64;
