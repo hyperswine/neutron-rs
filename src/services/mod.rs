a2d2630bd10b656d658742b45615b4cb73095a5c
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-16 19:36:12 +1100

    pdatesU

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -14,3 +14,7 @@ pub trait Service {
 }
 
 // WRITE TO DISPLAY BUFFER (VGA MONITOR DRIVER)
+// Need -display gtk (for a framebuffer I think or mostly controlling app level display options)
+// Maybe -vga cirrus or -vga std or -vga virtio
+
+// For hardware interrupts, https://github.com/riscv/riscv-aclint
