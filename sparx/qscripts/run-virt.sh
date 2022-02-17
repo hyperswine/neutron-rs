73556aa8d0ba050d7e95d69fe96b19af5e3f950b
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-17 16:56:08 +1100

    Change scripts

--- sparx/qscripts/run-virt.sh
+++ sparx/qscripts/run-virt.sh
@@ -12,11 +12,23 @@ cargo brv && echo "DONE COMPILING RUST"
 # assemble "bootloader" assembly
 $AS -c sparx/arch/riscv64/asm/entry.S -o build/entry.o && echo "DONE ASSEMBLING ASM"
 # link objects
-$LD -T sparx/arch/riscv64/link/linker.ld -nostdlib build/*.o build/*.a -o build/kernel.elf  && echo "DONE LINKING"
+$LD -T sparx/arch/riscv64/link/linker.ld -nostdlib build/*.o build/*.a -o build/kernel.elf && echo "DONE LINKING"
 
 QEMU="qemu-system-riscv64"
 echo "STARTING UP QEMU..."
-echo "USING: qemu-system-riscv64 -machine virt -bios build/kernel.elf -serial mon:stdio"
 
-# multiplex the virtual serial port (UART) and QEMU monitor into stdio (UART0)
-$QEMU -machine virt -bios build/kernel.elf -serial mon:stdio
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied, running with default params"
+    echo "RUN: qemu-system-riscv64 -machine virt -bios build/kernel.elf"
+    $QEMU -machine virt -bios build/kernel.elf
+fi
+
+if [[ -n "$1" ]] && [[ "${1#*.}" == "serial" ]]; then
+    echo "serial supplied, running with -serial mon:stdio"
+    $QEMU -machine virt -bios build/kernel.elf -serial mon:stdio
+fi
+
+if [[ -n "$1" ]] && [[ "${1#*.}" == "vga" ]]; then
+    echo "vga supplied, running with -vga std"
+    $QEMU -machine virt -bios build/kernel.elf -vga std
+fi
--- src/kernel/arch/riscv64gc/mod.rs
+++ src/kernel/arch/riscv64gc/mod.rs
@@ -1,8 +1,19 @@
 pub mod memory;
 pub mod power;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
