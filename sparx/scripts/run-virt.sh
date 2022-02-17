89f30685010697c765bfbe7ee212c9193de9d4a8
Jason Q <jiasheng.qin@student.unsw.edu.au>
2022-02-18 08:14:42 +1100

    UART0 now functional

--- sparx/scripts/run-virt.sh
+++ sparx/scripts/run-virt.sh
@@ -5,12 +5,17 @@
 
 AS="riscv64-unknown-elf-as"
 LD="riscv64-unknown-elf-ld"
+CARGO_CMD="brv"
+
+if [[ -n "$1" ]] && [[ "${1#*.}" == "arctest" ]]; then
+    CARGO_CMD="arctestb"
+fi
 
 # cleanup prev builds, note, all of them (can do build/riscv-virt, build/spectro, build/pi4 later)
 rm -rf build/*
 
 # compile rust code to library
-cargo brv && echo "DONE COMPILING RUST"
+cargo $CARGO_CMD && echo "DONE COMPILING RUST"
 # assemble "bootloader" assembly
 $AS -c sparx/arch/riscv64/asm/entry.S -o build/entry.o && echo "DONE ASSEMBLING ASM"
 # link objects
@@ -19,19 +24,9 @@ $LD -T sparx/arch/riscv64/link/linker.ld -nostdlib build/*.o build/*.a -o build/
 QEMU="qemu-system-riscv64"
 echo "STARTING UP QEMU..."
 
-if [ $# -eq 0 ]; then
+if [ $# -le 2 ]; then
     echo "No arguments supplied, running with default params"
-    echo "RUN: qemu-system-riscv64 -machine virt -bios build/kernel.elf"
-    $QEMU -machine virt -bios build/kernel.elf
-fi
-
-if [[ -n "$1" ]] && [[ "${1#*.}" == "serial" ]]; then
-    echo "serial supplied, running with -serial mon:stdio"
-    $QEMU -machine virt -bios build/kernel.elf -serial mon:stdio
-fi
-
-if [[ -n "$1" ]] && [[ "${1#*.}" == "virtio" ]]; then
-    echo "virtio supplied, running with -m 512M -serial mon:stdio -parallel none -vga virtio"
+    echo "RUN: qemu-system-riscv64 -machine virt -bios build/kernel.elf -m 512M -serial mon:stdio -parallel none -vga virtio"
     $QEMU -machine virt -bios build/kernel.elf -m 512M -serial mon:stdio -parallel none -vga virtio
 fi
 
