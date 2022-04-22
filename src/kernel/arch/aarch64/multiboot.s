004354c7e6b10e696feacf97fbc01c8a96e938e3
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-22 11:15:11 +1000

    Reorg comments

--- src/kernel/arch/aarch64/multiboot.s
+++ src/kernel/arch/aarch64/multiboot.s
@@ -1,18 +1,27 @@
 .section multiboot_header
 
-; before the ELF header, place header_start
+# before the ELF header, place header_start. Note, doubles are 32bits
 header_start:
-    .double 0xe85250d6                ; magic number (multiboot 2)
-    .double 0                         ; architecture 0 (protected mode i386)
-    .double header_end - header_start ; header length
-    ; checksum -> bootloader should verify by inverse op
+    # magic number (multiboot 2)
+    .double 0xe85250d6   
+
+    # architecture 1 (protected mode aarch64)
+    .double 0
+
+    # header length
+    .double header_end - header_start
+
+    # checksum -> bootloader should verify by inverse op
     .double 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-    ; insert optional multiboot tags here
+    # insert optional multiboot tags here
 
-    ; required end tag
-    .word 0    ; type
-    .word 0    ; flags
-    .double 8    ; size
+    # END TAG
+    # type
+    .word 0
+    # flags
+    .word 0
+    # size
+    .double 8
 
 header_end:
