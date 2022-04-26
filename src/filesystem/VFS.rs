e9f1a61bfdf848280acf0b397d566d8b5aedca7d
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-27 01:01:41 +1000

    Add more docs

---
+++ src/filesystem/VFS.rs
@@ -0,0 +1,65 @@
+// ----------------------
+// NEUTRON FS CENTRIC VFS
+// ----------------------
+
+// all filesystem drivers need to implement neutron vfs
+// all fs related syscalls and processes operate on vfs
+
+// FILE
+struct File;
+
+// -----
+// DIR
+// -----
+
+/*
+nefs-hierarchy.cfg
+
+# REQUIRED
+/sys
+    /dev
+        tty
+        null
+    /vars
+        path
+        local.pid_<pid>
+    /config
+        boot.cfg # basically .env format. Sets kernel modules to be loaded
+        init.cfg # sets daemons/services/sparx to be loaded as a list of paths to those executables. Prob in /sys/bin
+    init.elf
+    /bin
+        /network
+            __sparx_wifi
+            __sparx_eth
+            __sparx_bt
+            __sparx_dhcp
+            /server
+                __sparx_http
+                __sparx_ftp
+                __sparx_grpc
+        /io
+            __sparx_filesystem # rootfs service for managing reads and write queues and caches. Kinda like syncd
+            __sparx_devices # management of devices through managing queues and caches for /sys/dev
+        /log
+            __sparx_syslog
+        /time
+            __sparx_network_time
+        /cron
+            __sparx_cron
+        /system
+            __sparx_system_init # the main init daemon that spawns all other processes like the cli shell or DE. The DE then becomes the defacto spawner of GUI apps. But make no mistake, its still a child of system_init
+
+/desktop # DE + workspaces
+
+# RECOMMENDED
+/packages
+*/
+
+// the hierarchy depends on nefs-hierarchy.cfg
+struct RootFS;
+
+struct Dir;
+
+struct SymLink;
+
+struct Device;
--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -4,14 +4,49 @@
 // ARCH DEPENDENT STUFF
 // --------------------
 
-pub mod arch;
 pub mod acpi;
+pub mod arch;
 #[cfg(feature = "posix")]
 pub mod posix;
 
 // --------------------
 // NON-ARCH DEPENDENT STUFF
 // --------------------
+
+/*
+FINDING CURR EXCEPTION LEVEL
+
+.globl get_el
+get_el:
+    mrs x0, CurrentEL
+    lsr x0, x0, #2
+    ret
+
+CHANGING EXCEPTION LEVEL
+
+master:
+    # disabled if page tables dont exist yet, e.g. before bootloader sets it up
+    ldr    x0, =SCTLR_VALUE_MMU_DISABLED
+    msr    sctlr_el1, x0        
+
+    # hypervisor reg. Technically dont need but should have
+    ldr    x0, =HCR_VALUE
+    msr    hcr_el2, x0
+
+    # security register
+    ldr    x0, =SCR_VALUE
+    msr    scr_el3, x0
+
+    # saved program status reg. Execution level should be el3
+    ldr    x0, =SPSR_VALUE
+    msr    spsr_el3, x0
+
+    # return to this addr
+    adr    x0, el1_entry        
+    msr    elr_el3, x0
+
+    eret                
+*/
 
 use alloc::vec;
 
@@ -21,8 +56,21 @@ pub struct KernelManager {
     filesystem: Filesystem,
 }
 
+// Pass off to /sys/init. Make sure it exists. If it doesnt the whole thing should loop and show the error with a 10 second shutdown
 impl KernelManager {
-    fn k_main(&self) {
+    pub fn kernel_manager_entry(&self) -> ! {
+        loop {}
+    }
+
+    // basically init_entry. Set execution to ring 3/user mode
+    pub fn init(&self) -> ! {
+        // VFS load_process() into memory
+        // let pid = load_process("/sys/init")
+        // cpu.set_mode(U_MODE)
+        // this_thread.process_run(pid)
+        //  /sys/init should be able to spawn more threads using std library/neutron syscalls
+        //  and run in U mode
+
         loop {}
     }
 
