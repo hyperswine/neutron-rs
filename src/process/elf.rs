b134f49441197d72cc557801e3a59c51dc9df620
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-05-04 03:43:14 +1000

    Add more stuff

--- src/process/elf.rs
+++ src/process/elf.rs
@@ -1,8 +1,21 @@
 use goblin::*;
 
-pub fn load_elf(_file_contents: &str) -> Result<bool, ()>{
-    
-
-
-    Ok(true)
+pub struct Object;
+impl Object {
+    pub fn parse(contents: &str) {}
+    pub fn Elf(_: ()) {}
+    pub fn Unknown() {}
 }
+
+// pub fn load_elf(_file_contents: &str) -> Result<(), ()> {
+//     let data = match Object::parse(_file_contents) {
+//         Object::Elf(elf) => {
+//             elf
+//         }
+//         Object::Unknown(magic) => {
+//             ()
+//         }
+//     };
+
+//     Ok(data)
+// }
