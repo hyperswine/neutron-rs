aac2335d17ae802c666eb458eb37dac277698e9f
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-09 13:42:15 +1000

    modify debug.sh to work

--- .neutron/debug.sh
+++ .neutron/debug.sh
@@ -1,5 +1,4 @@
-file build/neutron_kernel
-gdb-remote localhost:1234
+lldb -ex file build/neutron_kernel -ex gdb-remote localhost:1234
 
-# NOTE: this only works for "pure builds". So for limine, it makes an iso and thus wont work
+# NOTE: this only works for "pure builds". So for limine, it makes an iso and thus wont work I think
 # Although maybe it could work once it actually boots in. But you'd have to do it manually
