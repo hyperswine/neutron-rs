7d6ce73b28780e241c2fc247d1b77cd917add855
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-13 14:19:20 +1000

    ADD!

--- neutron_kernel/src/arch/aarch64/entry.rs
+++ neutron_kernel/src/arch/aarch64/entry.rs
@@ -3,8 +3,8 @@
 // ---------------
 
 use crate::arch::aarch64::exception::init_interrupt_handlers;
-use core::{cell::UnsafeCell, fmt};
 use arcboot_api::ArcServices;
+use core::{cell::UnsafeCell, fmt};
 use cortex_a::{asm::barrier, registers::*};
 use log::info;
 use tock_registers::{
@@ -15,6 +15,17 @@ use tock_registers::{
 // -------------
 // SETUP
 // -------------
+
+// if page is unmapped flagged (page descriptor not there in L0,1,2) or just all 0s
+// ARM should pass the exception syndrome = page fault and what vaddr it faulted at or entry it faulted at
+pub fn page_fault_handler() {
+    // 6 bits telling us what kind of fault
+
+    // if all 0s, would prob be UNKNOWN descriptor fault
+    // then just get a new frame for the vaddr-page
+    // if a stack access, maybe allocate 2
+    // if a heap access, maybe allocate 16
+}
 
 // ? maybe pass the arc memory map here
 // maybe a field interrupt_table_start or vbar_start (virtually mapped into kernel's addr space)
