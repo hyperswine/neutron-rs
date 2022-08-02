a3c740829cf652cb2034e44168240a401e7f4aa2
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 09:19:02 +1000

    Update process to get rid of docs

--- neutron_kernel/src/process/mod.rs
+++ neutron_kernel/src/process/mod.rs
@@ -68,23 +68,10 @@ impl Process {
         }
     }
 
-    // NOTE: a process has 5 regions, https://en.wikipedia.org/wiki/File:Program_memory_layout.pdf
-    // Should return process exit code
+    /// A process has 5 regions, https://en.wikipedia.org/wiki/File:Program_memory_layout.pdf
+    /// Should return process exit code
     pub fn execute_elf64(&self, validated_elf_bin: &ElfBinary) -> ProcessExitStatus {
-        // load regions, request_pages is basically kmalloc()
-
-        // let text_pages = request_pages(validated_elf_bin.text_region_size())
-        // * Bss = uninitialised data, which should be in a bss region
-        // Bss is only good for reducing file size
-        // let bss_pages = request_pages(validated_elf_bin.bss_region_size())
-        // Data means global vars and static vars (local + global) which should be compiled & mangled to vars in .data
-        // let data_pages = request_pages(validated_elf_bin.data_region_size())
-        // let stack_pages = request_pages(5 * self.priority)
-        // let heap_pages = request_pages(5 * self.priority)
-
-        // ! when a process under userspace code control wants more memory, they must instead call malloc() rather than kmalloc()
-        // give control to the elf code, start at the top of the text segment
-        // this_thread.execute(USER_MODE, text_pages)
+        
 
         ProcessExitStatus::SUCCESS
     }
