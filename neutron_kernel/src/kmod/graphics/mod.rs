36f733d15532f3cc5ef454a9fc50f29d5eb50786
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 17:20:37 +1000

    Edit graphics kmod

--- neutron_kernel/src/kmod/graphics/mod.rs
+++ neutron_kernel/src/kmod/graphics/mod.rs
@@ -1,32 +1,4 @@
-// enable with #[cfg(feature = 'arcwm')]
+#[cfg(feature = "arcwm")]
 pub mod arcwm;
 // SHOULD BE ENABLED REGARDLESS
 pub mod kconsole;
-
-// ------------------------
-// USERSPACE GRAPHICS LAYER
-// ------------------------
-
-// Allow implementing of wgpu with these functions
-// ASSUME Either MPU/RTU or Mali GPUs
-
-// Primitive Assembly
-fn assemble_verts() {}
-
-// Vertex Shading
-fn shade_vertices() {}
-
-// Tessellate
-fn tessellate() {}
-
-// Geometry Shading
-fn shade_geometry() {}
-
-// Rasterise
-fn rasterize() {}
-
-// Fragment Shading
-fn shade_fragments() {}
-
-// Blend Colors
-fn blend_colors() {}
