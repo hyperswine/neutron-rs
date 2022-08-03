e40151509f5cb976867e3c11cf7109b09fa6d3c7
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 10:03:54 +1000

    add skel to vmo

---
+++ neutron_kernel/src/memory/vmo.rs
@@ -0,0 +1,2 @@
+/// A vm object that is backed up by some memory feature like a block of RAM, an MMIO region, or a CoW view. Can be readily mapped into a process' address space
+pub struct VirtualMemObject {}
