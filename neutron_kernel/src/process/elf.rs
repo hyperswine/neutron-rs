cc6b39f6b79904171e88632b182198816c9bb246
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 10:04:10 +1000

    edit elf launcher api

--- neutron_kernel/src/process/elf.rs
+++ neutron_kernel/src/process/elf.rs
@@ -2,20 +2,26 @@ use goblin::{
     container::{Container, Ctx},
     elf::{program_header, Elf, ProgramHeader},
 };
+use core::{arch::asm, intrinsics::transmute};
+use alloc::vec::Vec;
+use log::info;
 
+const ELF64_HDR_SIZE: usize = 64;
 pub const USERSPACE_STACK_START: u64 = 0x0000_FFFF_FFFF_FFFF;
 
 pub fn load_elf_userspace(elf_img_bytes: &[u8]) {
     let header =
-        Elf::parse_header(&kernel_img[..ELF64_HDR_SIZE]).map_err(|_| "parse elf header error");
+        Elf::parse_header(&elf_img_bytes[..ELF64_HDR_SIZE]).map_err(|_| "parse elf header error");
 
     let header = match header {
         Ok(h) => h,
         Err(err) => panic!("Error! {err}"),
     };
 
+    info!("header = {header:?}");
+
     let program_hdr_table_size = (header.e_phnum * header.e_phentsize) as usize;
-    let program_table = &kernel_img[ELF64_HDR_SIZE..ELF64_HDR_SIZE + program_hdr_table_size];
+    let program_table = &elf_img_bytes[ELF64_HDR_SIZE..ELF64_HDR_SIZE + program_hdr_table_size];
 
     let mut elf = Elf::lazy_parse(header)
         .map_err(|_| "cannot parse ELF file")
@@ -50,7 +56,7 @@ pub fn load_elf_userspace(elf_img_bytes: &[u8]) {
                 let offset = h.p_filesz as usize;
                 let count = h.p_offset as usize;
 
-                &kernel_img[offset..offset + count]
+                &elf_img_bytes[offset..offset + count]
             } else {
                 &[0; 1]
             }
@@ -60,7 +66,8 @@ pub fn load_elf_userspace(elf_img_bytes: &[u8]) {
     // Load sections
 
     elf.program_headers.iter().enumerate().for_each(|(ind, p)| {
-        map_segment(sections[ind], p.p_vaddr);
+        // map_segment(sections[ind], p.p_vaddr);
+        info!("Found a segment! {p:?}");
     });
 
     // Create a process addr space struct in the kernel for the process
@@ -70,7 +77,7 @@ pub fn load_elf_userspace(elf_img_bytes: &[u8]) {
     let entry = unsafe { transmute::<*const (), extern "C" fn()>(elf_entry_addr) };
 
     // arcboot api function
-    set_stack(USERSPACE_STACK_START);
+    // set_stack(USERSPACE_STACK_START);
 
     entry();
 }
