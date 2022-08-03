3ca60b431090c3cbc07fd5d5d4ecab48238bfb24
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 10:04:23 +1000

    add elf tests

--- neutron_kernel/tests/basic.rs
+++ neutron_kernel/tests/basic.rs
@@ -1,6 +1,10 @@
-use neutron_kernel::process::Process;
+use neutron_kernel::process::{elf::load_elf_userspace, Process};
+use std::fs;
+use simple_logger::SimpleLogger;
 
 extern crate std;
+
+// does a lazy_static work for log?
 
 #[test]
 fn test_sanity() {
@@ -17,4 +21,10 @@ fn test_elf_load() {
     // load an elf file from tests/elf into the function
     // see whether the function correctly identifies each section
     // and would load it to the right addrs
+    SimpleLogger::new().env().init().unwrap();
+    log::warn!("This is an example message.");
+
+    let f = fs::read("tests/elf/keygen_me").unwrap();
+
+    load_elf_userspace(&f);
 }
