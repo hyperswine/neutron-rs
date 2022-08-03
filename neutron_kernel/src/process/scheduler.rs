7ace704cc4450d968e1644f39b858f52ee46c7f5
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 17:34:46 +1000

    update scheduler

--- neutron_kernel/src/process/scheduler.rs
+++ neutron_kernel/src/process/scheduler.rs
@@ -1,3 +1,23 @@
+use super::Process;
+use alloc::vec::Vec;
+
+/// The default Scheduler API for Neutron
+pub struct KScheduler {
+    /// Could sort by id, name, space,
+    pub processes: Vec<Process>,
+}
+
+impl KScheduler {
+    pub fn new(processes: Vec<Process>) -> Self {
+        Self { processes }
+    }
+
+    /// Schedule the next lot of KThreads
+    pub fn tick(&mut self) {}
+}
+
+// NOTES:
+
 // SMP Scheduling
 
 // So you have m processes and n processors
@@ -8,3 +28,8 @@
 // So you have m processes and n processors
 // Some threads want to access resources which may be closer or farer away from a certain processor
 // A thread that wants to access a certain node should be scheduled on a processor node nearer to the resource. Need some API in the driver or scheduler/interrupt handler that determines it
+
+// USERSPACE Scheduling
+
+// via something like schedulerd/spx:sched
+// And kernel maps its privileged vmobject containing a list of processes into the proc addr space
