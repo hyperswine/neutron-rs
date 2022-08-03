01991f3088cfc37e51b0288e9ef3e7125492d4a4
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 17:20:30 +1000

    Del spectral rt from kernelspace

--- neutron_kernel/src/kmod/graphics/rt.rs
+++
@@ -1,8 +0,0 @@
-// -----------------
-// Spectral RayTrace
-// -----------------
-
-// 1. convert a RayTrace scene description (.yml) through compiler into .rt binary
-// 2. convert a cpu game program into ELF. When the functions (these functions below) are called, copy command buffer and tell RTU to ray trace
-
-fn cast_ray() {}
